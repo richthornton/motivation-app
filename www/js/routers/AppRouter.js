ce2b16d94d6142ce76b34c8ec8a1a4883222a0f7
Richard Thornton <richardthornton01@gmail.com>
2015-02-15 13:35:43 +0000

    Changes to navbar, still needs more to stop grey ovelay of the actual links in the slide in nav

--- www/js/routers/AppRouter.js
+++ www/js/routers/AppRouter.js
@@ -8,6 +8,14 @@ app.routers.AppRouter = Backbone.Router.extend({
 
     initialize: function () {
         app.slider = new PageSlider($('body'));
+
+        app.headerView = new app.views.HeaderView();
+        app.headerView.render();
+        app.slider.slidePage(app.headerView.$el);
+
+        $( document ).ready(function(){
+            $(".button-collapse").sideNav();
+        }) 
     },
 
     home: function () {
@@ -27,7 +35,7 @@ app.routers.AppRouter = Backbone.Router.extend({
     },
 
     favourite: function () {
-
+        
     }
 
 });
